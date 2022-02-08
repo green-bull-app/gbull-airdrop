02da4ba42b4e5809f026b259e63d303e048f0ca3
The Bull <info@greenbull.app>
2022-02-08 10:34:50 +0000

    Add functions to impersonate a whale account.

---
+++ test/airdrop.js
@@ -0,0 +1,75 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+let token, signer, accounts;
+const gbullTokenAddress = '0x21ecEa8f7788808848fc61D3FB8897F1a9a00D94';
+const whaleAccountAddress = '0x63EcB27C684f148910d178221EDaEBDDb9BBAAaF';
+
+const baseTokenContract = () => {
+  const baseTokenAbi = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function balanceOf(address) view returns (uint)",
+    "function allowance(address _owner, address spender) external view returns (uint)",
+    "function approve(address spender, uint amount) external",
+    "function transfer(address recipient, uint amount) external"
+  ];
+  return new ethers.Contract(gbullTokenAddress, baseTokenAbi, hre.ethers.provider);
+}
+
+const DeployAirdropContract = async () => {
+  const Airdrop = await ethers.getContractFactory("Airdrop");
+  const airdrop = await Airdrop.deploy(gbullTokenAddress, BigInt(5000 * 10 ** 18).toString());
+  await airdrop.deployed();
+
+  return airdrop;
+}
+
+const ImpersonateAccount = async (accounts) => {
+  await hre.network.provider.request({
+    method: "hardhat_impersonateAccount",
+    params: [accounts],
+  });
+}
+
+const AddFundsToAirDropContract = async (airdrop) => {
+  const airdropAmount = BigInt(5000000);
+  await ImpersonateAccount(whaleAccountAddress);
+  const whaleSigner = await hre.ethers.provider.getSigner(whaleAccountAddress);
+  const tx = await baseTokenContract().connect(whaleSigner).transfer(airdrop.address, airdropAmount.toString());
+  await tx.wait();
+  const airdropContractBalance = BigInt(await baseTokenContract().balanceOf(airdrop.address));
+  console.log(airdropContractBalance)
+}
+
+describe("Airdrop", function () {
+  it("Should test if airdrops can be claimed before the specified date", async function () {
+    const airdrop = await DeployAirdropContract();
+    await AddFundsToAirDropContract(airdrop);
+
+    // expect(await airdrop.greet()).to.equal("Hello, world!");
+    //
+    // const setGreetingTx = await airdrop.setGreeting("Hola, mundo!");
+    //
+    // // wait until the transaction is mined
+    // await setGreetingTx.wait();
+    //
+    // expect(await airdrop.greet()).to.equal("Hola, mundo!");
+  });
+
+  it("Should test if airdrops can be claimed after the specified date", async function () {
+
+  });
+
+  it("Should test if users can claim airdrop", async function () {
+
+  })
+
+  it("Should test if users can claim airdrop twice", async function () {
+
+  });
+
+  it("Should test if ineligible users can claim airdrop", async function () {
+
+  })
+});
